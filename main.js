affd8c7f167bd4c12c70ee42038d76c11fa2c8d1
Jevaughn Vickers <fullstackjev@gmail.com>
2023-06-16 17:58:47 -0400

    create UI

--- main.js
+++ main.js
@@ -1,13 +1,26 @@
+let gridContainer = document.getElementById("container");
+let numbers = document.getElementById("numbers");
+
+let slider = document.getElementById("slider");
+
+
+slider.addEventListener
+
+let rowSize = slider.value;
+let gridSize = rowSize ** 2;
+
+
 const divArray = [];
-for(let i = 0;i < 256;i++){
+for(let i = 0;i < gridSize;i++){
     let div = document.createElement("div");
     divArray.push(div);
 }
 
-
-let gridContainer = document.getElementById("container");
+numbers.textContent = `${rowSize} x ${rowSize}`;
 
 for(let div of divArray){
     div.classList.add("grid-squares");
     gridContainer.appendChild(div)
 }
+
+
